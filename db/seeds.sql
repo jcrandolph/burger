09e66460bb5efae71daf9f153a72fac1926c8c62
jcrandolph <jackcrandi@gmail.com>
2019-03-17 22:32:39 -0400

    setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cali');
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Turkey');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo');
---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = 3000;
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
